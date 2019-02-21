4caa7a8c3a82dcb2650432589abab460f8441d09
Peggy Liu <peggyliu@Peggys-MacBook-Air-3.local>
2019-02-21 13:47:52 -0500

    Initial Site Files

---
+++ assets/javascript/app.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+    
+    
+        $('#countdown').hide();
+        $('.trivia-ques').hide();
+        $('.results').hide();
+    
+        
+        var number = 180; 
+        var intervalId;
+        var correctCount = 0;
+        var wrongCount = 0;
+        var unanswered = 0;
+    
+    
+    
+        
+        function showQuestions(){
+            $('#countdown').show();
+            $('.trivia-ques').show();
+            $('#game-done').show();
+        }
+    
+        
+        function countdownTimer(){
+                intervalId = setInterval(decrement, 1000);
+        }
+       
+            
+        function decrement(){
+            number--;
+            $('#timer').html(" " + number + " " + "seconds");
+            if (number ===1){
+                $('#timer').html(" " + number + " " + "second");
+            }
+            else if(number ===0) {
+                stop();
+                hide();
+                displaySummary();
+            }
+        }
+    
+            
+        function stop() {
+            clearInterval(intervalId);
+        }
+    
+            
+        function hide(){
+            $('#countdown').hide();
+            $('.trivia-ques').hide();
+            $('#game-done').hide();
+        }
+    
+        
+        function displaySummary(){
+            $('.results').show();
+            unanswered = (8-(correctCount+wrongCount));
+            $('#correctScore').text("Correct Answers:" + " " + correctCount); 
+            $('#wrongScore').text("Wrong Answers:" + " " + wrongCount); 
+            $('#unanswered').text("Unanswered:" + " " + unanswered); 
+        }
+    
+    
+    
+        
+        $('#game-start').on('click', function(){
+            $('#game-start').hide();
+            showQuestions();
+            countdownTimer();
+        }); 
+    
+        
+        $('#game-done').on('click', function(){
+            $('#game-start').hide(); 
+            hide();
+            displaySummary();
+        });
+    
+        
+        $('input[type=radio]').on ('change', function(){
+        correctCount = $('input[value=correct]:checked').length;
+        wrongCount = $('input[value=wrong]:checked').length;
+        unanswered = (8-(correctCount+wrongCount));
+        });
+    
+    
+    });
